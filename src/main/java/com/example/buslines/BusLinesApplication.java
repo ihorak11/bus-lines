dcf1c835ccf6262bbbf54fc89977050753d3cb91
Ivan Horak <ivan.horak@ericsson.com>
2022-07-26 20:53:08 +0200

    Initial commit

---
+++ src/main/java/com/example/buslines/BusLinesApplication.java
@@ -0,0 +1,13 @@
+package com.example.buslines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusLinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusLinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/buslines/BusLinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buslines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusLinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
