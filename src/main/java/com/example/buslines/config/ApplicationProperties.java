c34f41e9fd3602febb6d4ac15173291bc46a39f8
Ivan Horak <ivan.horak@ericsson.com>
2022-07-26 21:15:02 +0200

    Add SL API url to application.properties. Add ApplicationProperties class for ease of access

---
+++ src/main/java/com/example/buslines/config/ApplicationProperties.java
@@ -0,0 +1,18 @@
+package com.example.buslines.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@PropertySource(value = "classpath:application.properties")
+public class ApplicationProperties {
+
+    @Value("${api.sl.url}")
+    private String apiSlUrl;
+
+    @Value("${api.sl.api-key}")
+    private String apiKeySl;
+}
