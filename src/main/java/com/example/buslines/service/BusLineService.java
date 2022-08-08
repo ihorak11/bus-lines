18b9ea7aca55a0a7f832d0df442d90de016dfeee
Ivan Horak <ivan.horak@ericsson.com>
2022-08-08 21:04:54 +0200

    Introduce BusLineUtils to make testing easier to maintain

--- src/main/java/com/example/buslines/service/BusLineService.java
+++ src/main/java/com/example/buslines/service/BusLineService.java
@@ -1,103 +1,29 @@
 package com.example.buslines.service;
 
 import com.example.buslines.api.client.SLApiClient;
-import com.example.buslines.model.BusLine;
 import com.example.buslines.api.response.JourneyLineStopPoint;
 import com.example.buslines.api.response.JourneyPatternStopResponse;
-import com.example.buslines.model.LineDirectionPair;
-import com.example.buslines.api.response.StopPointDetails;
 import com.example.buslines.model.TopBusLinesScoreboard;
+import com.example.buslines.util.BusLineUtils;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
 public class BusLineService {
     //TODO put helpful comments everywhere once code is done
     private SLApiClient slApiClient;
-
-    private final String DELIMITER = ";";
+    private BusLineUtils busLineUtils;
 
     public TopBusLinesScoreboard getTop10BusLines() {
         JourneyPatternStopResponse journeyResponse = slApiClient.getSlBusLines();
         ArrayList<JourneyLineStopPoint> stopPointList = journeyResponse.getJourneyResponseData().getJourneyLineStopPointList();
-        Map<String, Set<String>> busLineToStopIdMap = extractBusLineStopIdMap(stopPointList);
+        Map<String, Set<String>> busLineToStopIdMap = busLineUtils.extractBusLineStopIdMap(stopPointList);
 
-        return createTopBusLinesScoreboardObject(busLineToStopIdMap);
-    }
-
-    /**
-     * @param journeyLineStopPointList List of stop point details together with details about which bus stop they belong to
-     * @return Map where the key is the bus identification string in format "lineNumber;directionCode" and the values are a set of unique stop IDs
-     */
-    private Map<String, Set<String>> extractBusLineStopIdMap(ArrayList<JourneyLineStopPoint> journeyLineStopPointList) {
-        return journeyLineStopPointList.stream()
-                .collect(
-                        Collectors.groupingBy(stopPoint -> stopPoint.getLineNumber() + DELIMITER + stopPoint.getDirectionCode(),
-                                Collectors.mapping(JourneyLineStopPoint::getJourneyPatternPointNumber, Collectors.toSet()))
-                );
-    }
-
-    /**
-     * @param busLineToStopIdMap Map where the key is the bus identification string in format "lineNumber;directionCode" and the values are a set of unique stop IDs
-     * @return Fully prepared object that will be consumed by frontend to construct the bus line scoreboard
-     */
-    private TopBusLinesScoreboard createTopBusLinesScoreboardObject(Map<String, Set<String>> busLineToStopIdMap) {
-        Map<String, Set<String>> topTenBusLines = busLineToStopIdMap.entrySet().stream()
-                .sorted(Comparator.comparing(o -> o.getValue().size(), Comparator.reverseOrder()))
-                .limit(10)
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-
-        //the API seems to not return the details for all the stops, resulting in inability to extract all of the stop names from the response
-        ArrayList<StopPointDetails> stopPointDetailsList = slApiClient.getStopPointDetails().getResponseData().getStopPointDetailsList();
-        LineDirectionPair lineDirectionPair;
-        BusLine busLine;
-        ArrayList<BusLine> busLines = new ArrayList<>();
-
-        for (String key : topTenBusLines.keySet()) {
-            lineDirectionPair = getLineDirectionPair(key);
-            busLine = BusLine.builder()
-                    .designation(lineDirectionPair.getLineNumber())
-                    .direction(lineDirectionPair.getDirection())
-                    .stopNames(getStopNamesFromStopIds(stopPointDetailsList, topTenBusLines.get(key)))
-                    .noOfStops(topTenBusLines.get(key).size())
-                    .build();
-
-            busLines.add(busLine);
-        }
-
-        busLines.sort(Comparator.comparing(BusLine::getNoOfStops, Comparator.reverseOrder()));
-
-        return TopBusLinesScoreboard.builder()
-                .busLines(busLines)
-                .build();
-    }
-
-    /**
-     * @param stopPointDetailsList List of objects containing bus stop details
-     * @param stopPointIdSet Set of stop IDs for a specific bus line
-     * @return Set of bus stop names
-     */
-    private Set<String> getStopNamesFromStopIds(ArrayList<StopPointDetails> stopPointDetailsList, Set<String> stopPointIdSet) {
-
-        return stopPointDetailsList.stream()
-                .filter(stopPointDetails -> stopPointIdSet.contains(stopPointDetails.getStopPointNumber()))
-                .map(StopPointDetails::getStopPointName)
-                .collect(Collectors.toSet());
-    }
-
-    private LineDirectionPair getLineDirectionPair(String lineDirectionKey) {
-        String[] split = lineDirectionKey.split(DELIMITER);
-
-        return LineDirectionPair.builder()
-                .lineNumber(split[0])
-                .direction(split[1])
-                .build();
+        return busLineUtils.createTopBusLinesScoreboardObject(busLineToStopIdMap);
     }
 }
