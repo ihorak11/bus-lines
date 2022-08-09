c6224a5301306fab0cde29afa419195f5a1b1860
Ivan Horak <ivan.horak@ericsson.com>
2022-08-09 10:28:39 +0200

    Remove redundant application.properties source definition

--- src/main/java/com/example/buslines/config/ApplicationProperties.java
+++ src/main/java/com/example/buslines/config/ApplicationProperties.java
@@ -2,12 +2,10 @@ package com.example.buslines.config;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
 @Getter
-@PropertySource(value = "classpath:application.properties")
 public class ApplicationProperties {
 
     @Value("${api.sl.url}")
